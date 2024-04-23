e2c8f0af55d7b0e4355e15467bc8212595327fcd
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-04-23 20:40:04 +0200

    Set the advertising packet data

--- lesson2/blefund_less2_exer1/src/main.c
+++ lesson2/blefund_less2_exer1/src/main.c
@@ -5,7 +5,8 @@
 static const struct bt_data ad[] = {
     /*Set the advertising flags */
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    /**/
+    /*Set the advertising packet data*/
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 
 
-}
+};
