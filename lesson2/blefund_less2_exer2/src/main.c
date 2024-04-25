915c2d101c2f2e4554faba45b74ce03f5d49eb98
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-04-25 12:18:25 +0200

    Include the Manufacturer Specific Data in the advertising packet

--- lesson2/blefund_less2_exer2/src/main.c
+++ lesson2/blefund_less2_exer2/src/main.c
@@ -34,10 +34,10 @@ static struct bt_le_adv_param *adv_param =
 
 /*Declare the advertising packet */
     static const struct bt_data ad[] = {
-    /*Set the advertising flags */
     BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-    /*Set the advertising packet data*/
     BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+    // Include the Manufacturer Specific Data in the advertising packet.
+    BT_DATA(BT_DATA_MANUFACTURE, (unsigned char *) &adv_mfg_data, sizeof(adv_mfg_data)),
 };
 
 /*Declare the URL data to include in the scan response */
@@ -49,6 +49,9 @@ static const struct bt_data sd[] = {
     /*Include the URL data in the scan response packet*/
     BT_DATA(BT_DATA_URI,url_data,sizeof(url_data)),
 };
+
+
+
 
 void main(void){
 
