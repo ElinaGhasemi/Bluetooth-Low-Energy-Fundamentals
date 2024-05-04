56ed93969d345c8c2ce29a66b270992155f1f207
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-05-04 16:24:09 +0200

    Set up callbacks

--- lesson3/blefund_less3_exer1/src/main.c
+++ lesson3/blefund_less3_exer1/src/main.c
@@ -4,10 +4,11 @@
 #include <zephyr/bluetooth/gap.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/addr.h>
+#include <zephyr/bluetooth/conn.h>
 
 #include <dk_buttons_and_leds.h>
 
-LOG_MODULE_REGISTER(Lesson2_Exercise3, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) -1)
@@ -15,12 +16,15 @@ LOG_MODULE_REGISTER(Lesson2_Exercise3, LOG_LEVEL_INF);
 #define RUN_STATUS_LED DK_LED1
 #define RUN_LED_BLINK_INTERVAL 1000
 
+struct bt_conn *my_conn = NULL;
+
 // Create the advertising parameter for connectable advertising
 static struct bt_le_adv_param *adv_param = 
     BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_USE_IDENTITY, /* Connectable advertising and use identity address */
     800,  /* Min Advertising Interval 500ms (800*0.625ms) */
     801,  /* Max Advertising Interval 500.625ms (801*0.625ms) */
     NULL); /* Set to NULL for undirected advertising */
+struct bt_conn *my_conn = NULL;
 
 /*Declare the advertising packet */
     static const struct bt_data ad[] = {
@@ -34,6 +38,23 @@ static const struct bt_data sd[] = {
     BT_DATA_BYTES(BT_DATA_UUID128_ALL,BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)),
 };
 
+/*Implement the callback functions */
+void on_connected(struct bt_conn *conn, uint8_t err)
+{
+    if (err)
+    {
+        LOG_ERR("Connection error %d",err);
+    }
+    LOG_INF("Connected");
+    my_conn = bt_conn_ref(conn);
+    
+}
+
+/*Declare the connection_callback structure */
+struct bt_conn_cb connection_callbacks = {
+    .connected = on_connected,
+    .discoonected = on_disconnected, 
+};
 
 
 void main(void){
