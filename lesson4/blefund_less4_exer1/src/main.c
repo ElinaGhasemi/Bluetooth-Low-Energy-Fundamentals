ad5191516d7a176916920606b89a6acdd76bf629
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-07-28 18:01:22 +0200

    Include the necessary header files

---
+++ lesson4/blefund_less4_exer1/src/main.c
@@ -0,0 +1,8 @@
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/conn.h>
+
+#include <dk_buttons_and_leds.h>
