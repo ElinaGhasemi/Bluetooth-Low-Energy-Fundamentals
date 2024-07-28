facda658a87734ac468d812c9cbb145760cc4b97
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-07-28 18:06:24 +0200

    Some necessary defines

--- lesson4/blefund_less4_exer1/src/main.c
+++ lesson4/blefund_less4_exer1/src/main.c
@@ -15,3 +15,11 @@ static struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM(
 	NULL); /* Set to NULL for undirected advertising */
 
 LOG_MODULE_REGISTER(Lesson4_Exercise1, LOG_LEVEL_INF);
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+#define RUN_STATUS_LED DK_LED1
+#define CON_STATUS_LED DK_LED2
+
+#define RUN_LED_BLINK_INTERVAL 1000
