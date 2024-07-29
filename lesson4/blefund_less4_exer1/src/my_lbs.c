37146daea7da1e2c2a9553aef2439696e3dd40ee
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-07-29 16:11:34 +0200

    Implement the read callback function of the Button characteristic

--- lesson4/blefund_less4_exer1/src/my_lbs.c
+++ lesson4/blefund_less4_exer1/src/my_lbs.c
@@ -28,6 +28,21 @@ static struct my_lbs_cb lbs_cb;
 /* Implement the write callback function of the LED characteristic */
 
 /* Implement the read callback function of the Button characteristic */
+static ssize_t read_button(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf,
+			   uint16_t len, uint16_t offset)
+{
+	// get a pointer to button_state which is passed in the BT_GATT_CHARACTERISTIC() and stored in attr->user_data
+	const char *value = attr->user_data;
+	LOG_DBG("Attribute read, handle: %u, conn: %p", attr->handle, (void *)conn);
+	if (lbs_cb.button_cb)
+	{
+		// Call the application callback function to update the get the current value of the button
+		button_state = lbs_cb.button_cb();
+		return bt_gatt_attr_read(conn, attr, buf, len, offset, value, sizeof(*value));
+	}
+	return 0;
+
+}
 
 /* LED Button Service Declaration */
 /* Create and add the MY LBS service to the Bluetooth LE stack */
@@ -37,7 +52,8 @@ BT_GATT_SERVICE_DEFINE(my_lbs_svc, BT_GATT_PRIMARY_SERVICE(BT_UUID_LBS),
 					      BT_GATT_PERM_READ, read_button, NULL, &button_state),
 		       /* Create and add the LED characteristic. */
 		       BT_GATT_CHARACTERISTIC(BT_UUID_LBS_LED, BT_GATT_CHRC_WRITE,
-					      BT_GATT_PERM_WRITE, NULL, write_led, NULL),					      
+					      BT_GATT_PERM_WRITE, NULL, write_led, NULL),		
+);					 			      
 
 /* A function to register application callbacks for the LED and Button characteristics  */
 int my_lbs_init(struct my_lbs_cb *callbacks)
