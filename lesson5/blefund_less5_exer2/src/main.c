6c9cb9e7aecec1bd59571586d42a1dae99436423
fatemehGhS <fatemeh.ghasemi.sm@gmail.com>
2024-08-15 21:48:45 +0200

    Define advertising parameter for when Accept List is used

--- lesson5/blefund_less5_exer2/src/main.c
+++ lesson5/blefund_less5_exer2/src/main.c
@@ -33,7 +33,11 @@ LOG_MODULE_REGISTER(Lesson5_Exercise2, LOG_LEVEL_INF);
 #define BT_LE_ADV_CONN_NO_ACCEPT_LIST                                                              \
 	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_ONE_TIME,                        \
 			BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
-/* STEP 3.2.2 - Define advertising parameter for when Accept List is used */
+/* Define advertising parameter for when Accept List is used */
+#define BT_LE_ADV_CONN_ACCEPT_LIST                                                                 \
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_FILTER_CONN |                    \
+				BT_LE_ADV_OPT_ONE_TIME,                                            \
+			BT_GAP_ADV_FAST_INT_MIN_2, BT_GAP_ADV_FAST_INT_MAX_2, NULL)
 
 static bool app_button_state;
 
